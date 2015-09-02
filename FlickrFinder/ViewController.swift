2ae4a63f2a92788acf00f78bc8633cafb0cf6cee
Dennis <denniszelada@gmail.com>
2015-09-01 21:22:51 -0300

    implemented the search by text and latitud longitud

--- FlickrFinder/ViewController.swift
+++ FlickrFinder/ViewController.swift
@@ -16,6 +16,13 @@ let EXTRAS = "url_m"
 let SAFE_SEARCH = "1"
 let DATA_FORMAT = "json"
 let NO_JSON_CALLBACK = "1"
+let BOUNDING_BOX_HALF_WIDTH = 1.0
+let BOUNDING_BOX_HALF_HEIGHT = 1.0
+let LAT_MIN = -90.0
+let LAT_MAX = 90.0
+let LON_MIN = -180.0
+let LON_MAX = 180.0
+
 
 class ViewController: UIViewController {
 
@@ -26,11 +33,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var latitudTextField: UITextField!
     @IBOutlet weak var longitudTextField: UITextField!
     
+    var tapRecognizer: UITapGestureRecognizer? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tapRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        tapRecognizer?.numberOfTapsRequired = 1
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.addKeyboardDismissRecognizer()
+        self.subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.removeKeyboardDismissRecognizer()
+        self.unsubscribeToKeyboarNotifications()
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -41,7 +65,7 @@ class ViewController: UIViewController {
         let methodArguments = [
             "method": METHOD_NAME,
             "api_key": API_KEY,
-            "text": "baby asian elephant",
+            "text": self.phraseTextField.text,
             "safe_search": SAFE_SEARCH,
             "extras": EXTRAS,
             "format": DATA_FORMAT,
@@ -50,6 +74,21 @@ class ViewController: UIViewController {
         getImageFromFlickrBySearch(methodArguments)
     }
 
+    @IBAction func searchPhotosByLatLonButtonTouchUp(sender: AnyObject) {
+        
+        let methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "bbox": createBoundingBoxString(),
+            "safe_search": SAFE_SEARCH,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        getImageFromFlickrBySearch(methodArguments)
+    }
+    
+    
     func getImageFromFlickrBySearch (methodArguments: [String: AnyObject]) {
         let session = NSURLSession.sharedSession()
         let urlString = BASE_URL + escapedParameters(methodArguments)
@@ -62,7 +101,49 @@ class ViewController: UIViewController {
             } else {
                 var parsingError: NSError? = nil
                 let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as? NSDictionary
-                println(parsedResult)
+                
+                if let photosDictionary = parsedResult?.valueForKey("photos") as? [String: AnyObject] {
+                    var totalPhotosVal = 0
+                    if let totalPhotos = photosDictionary["total"] as? String {
+                        totalPhotosVal = (totalPhotos as NSString).integerValue
+                    }
+                    
+                    if totalPhotosVal > 0 {
+                        if let photosArray = photosDictionary["photo"] as? [[String: AnyObject]] {
+                            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photosArray.count)))
+                            let photoDictionary = photosArray[randomPhotoIndex] as [String: AnyObject]
+                            
+                            /* prepare the uiview for updates*/
+                            var photoTitle = photoDictionary["title"] as? String
+                            let imageUrlString = photoDictionary["url_m"] as? String
+                            let imageURL = NSURL(string: imageUrlString!)
+
+
+                            /* update the UI on the main thread */
+                            if let imageData = NSData(contentsOfURL: imageURL!){
+                                dispatch_async(dispatch_get_main_queue(), {
+                                    self.defaultLabel.alpha = 0.0
+                                    self.photoImageView.image = UIImage(data: imageData)
+                                    self.photoTitleLabel.text = "\(photoTitle!)"
+                                })
+                            } else {
+                                println("Image does not exist at \(imageURL)")
+                            }
+                        } else {
+                            println("Can't find key 'photo' in \(photosDictionary)")
+                        }
+                    } else {
+                        dispatch_async(dispatch_get_main_queue(), {
+                            self.photoTitleLabel.text = "No Photos Found. Search Again"
+                            self.defaultLabel.alpha = 1.0
+                            self.photoImageView.image = nil
+                        })
+                    }
+                    
+                } else {
+                    println("Can't find key photos in \(parsedResult)")
+                }
+                
             }
         }
         task.resume()
@@ -90,7 +171,58 @@ class ViewController: UIViewController {
         return (!urlVars.isEmpty ? "?" : "") + join("&", urlVars)
     }
 
+    func addKeyboardDismissRecognizer() {
+        self.view.addGestureRecognizer(tapRecognizer!)
+    }
+    
+    func removeKeyboardDismissRecognizer() {
+        self.view.removeGestureRecognizer(tapRecognizer!)
+    }
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    func subscribeToKeyboardNotifications(){
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:"    , name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:"    , name: UIKeyboardWillHideNotification, object: nil)
 
+    }
+    
+    func unsubscribeToKeyboarNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:
+            UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:
+            UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if self.photoImageView.image != nil {
+            self.defaultLabel.alpha = 0.0
+        }
+            view.frame.origin.y += getKeyboardHeight(notification)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if self.photoImageView.image != nil {
+            self.defaultLabel.alpha = 0.0
+        }
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func createBoundingBoxString() -> String {
+        let latitud = (self.latitudTextField.text as NSString).doubleValue
+        let longitud = (self.longitudTextField.text as NSString).doubleValue
+        return "\(longitud - BOUNDING_BOX_HALF_WIDTH),\(latitud - BOUNDING_BOX_HALF_HEIGHT),\(longitud + BOUNDING_BOX_HALF_WIDTH),\(latitud + BOUNDING_BOX_HALF_HEIGHT)"
+    }
     
 }
 
