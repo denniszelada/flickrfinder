7a0527ed366219b4f5e5165ce9c1464feb2c5f29
Dennis <denniszelada@gmail.com>
2015-09-02 21:52:47 -0300

    adding validations for empty values

--- FlickrFinder/ViewController.swift
+++ FlickrFinder/ViewController.swift
@@ -24,14 +24,20 @@ let LON_MIN = -180.0
 let LON_MAX = 180.0
 
 
+extension String {
+    func toDouble() -> Double? {
+        return NSNumberFormatter().numberFromString(self)?.doubleValue
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var defaultLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var phraseTextField: UITextField!
     @IBOutlet weak var photoTitleLabel: UILabel!
-    @IBOutlet weak var latitudTextField: UITextField!
-    @IBOutlet weak var longitudTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
     
     var tapRecognizer: UITapGestureRecognizer? = nil
     
@@ -62,30 +68,59 @@ class ViewController: UIViewController {
 
     
     @IBAction func searchPhotosByPhraseButtonTouchUp(sender: AnyObject) {
-        let methodArguments = [
-            "method": METHOD_NAME,
-            "api_key": API_KEY,
-            "text": self.phraseTextField.text,
-            "safe_search": SAFE_SEARCH,
-            "extras": EXTRAS,
-            "format": DATA_FORMAT,
-            "nojsoncallback": NO_JSON_CALLBACK
-        ]
-        getImageFromFlickrBySearch(methodArguments)
+        self.dismissAnyVisibleKeyboards()
+        
+        if !self.phraseTextField.text.isEmpty {
+            self.photoTitleLabel.text = "Searching..."
+            let methodArguments = [
+                "method": METHOD_NAME,
+                "api_key": API_KEY,
+                "text": self.phraseTextField.text,
+                "safe_search": SAFE_SEARCH,
+                "extras": EXTRAS,
+                "format": DATA_FORMAT,
+                "nojsoncallback": NO_JSON_CALLBACK
+            ]
+            getImageFromFlickrBySearch(methodArguments)
+        } else {
+            self.photoTitleLabel.text = "Phrase Empty."
+        }
     }
 
     @IBAction func searchPhotosByLatLonButtonTouchUp(sender: AnyObject) {
+        self.dismissAnyVisibleKeyboards()
         
-        let methodArguments = [
-            "method": METHOD_NAME,
-            "api_key": API_KEY,
-            "bbox": createBoundingBoxString(),
-            "safe_search": SAFE_SEARCH,
-            "extras": EXTRAS,
-            "format": DATA_FORMAT,
-            "nojsoncallback": NO_JSON_CALLBACK
-        ]
-        getImageFromFlickrBySearch(methodArguments)
+        if !self.latitudeTextField.text.isEmpty && !self.longitudeTextField.text.isEmpty {
+            if validLatitude() && validLongitude() {
+                self.photoTitleLabel.text = "Searching..."
+            let methodArguments = [
+                "method": METHOD_NAME,
+                "api_key": API_KEY,
+                "bbox": createBoundingBoxString(),
+                "safe_search": SAFE_SEARCH,
+                "extras": EXTRAS,
+                "format": DATA_FORMAT,
+                "nojsoncallback": NO_JSON_CALLBACK
+            ]
+            getImageFromFlickrBySearch(methodArguments)
+            } else {
+                if !validLatitude() && !validLongitude() {
+                    self.photoTitleLabel.text = "Lat/Lon Invalid.\nLat should be [-90, 90].\nLon should be [-180, 180]."
+                } else if !validLatitude() {
+                    self.photoTitleLabel.text = "Lat Invalid.\nLat should be [-90, 90]."
+                } else {
+                    self.photoTitleLabel.text = "Lon Invalid.\nLon should be [-180, 180]."
+                }
+            }
+        } else {
+            if self.latitudeTextField.text.isEmpty && self.longitudeTextField.text.isEmpty {
+                self.photoTitleLabel.text = "Lat/Lon Empty."
+            } else if self.latitudeTextField.text.isEmpty {
+                self.photoTitleLabel.text = "Lat Empty."
+            } else {
+                self.photoTitleLabel.text = "Lon Empty."
+            }
+        }
     }
     
     
@@ -192,9 +227,33 @@ class ViewController: UIViewController {
         task.resume()
     }
     
+    /* Check to make sure the latitude falls within [-90, 90] */
+    func validLatitude() -> Bool {
+        if let latitude : Double? = self.latitudeTextField.text.toDouble() {
+            if latitude < LAT_MIN || latitude > LAT_MAX {
+                return false
+            }
+        } else {
+            return false
+        }
+        return true
+    }
+    
+    /* Check to make sure the longitude falls within [-180, 180] */
+    func validLongitude() -> Bool {
+        if let longitude : Double? = self.longitudeTextField.text.toDouble() {
+            if longitude < LON_MIN || longitude > LON_MAX {
+                return false
+            }
+        } else {
+            return false
+        }
+        return true
+    }
+    
     func getLatLonString() -> String {
-        let latitude = (self.latitudTextField.text as NSString).doubleValue
-        let longitude = (self.longitudTextField.text as NSString).doubleValue
+        let latitude = (self.latitudeTextField.text as NSString).doubleValue
+        let longitude = (self.longitudeTextField.text as NSString).doubleValue
         
         return "(\(latitude), \(longitude))"
     }
@@ -267,10 +326,21 @@ class ViewController: UIViewController {
     }
     
     func createBoundingBoxString() -> String {
-        let latitud = (self.latitudTextField.text as NSString).doubleValue
-        let longitud = (self.longitudTextField.text as NSString).doubleValue
+        let latitud = (self.latitudeTextField.text as NSString).doubleValue
+        let longitud = (self.longitudeTextField.text as NSString).doubleValue
         return "\(longitud - BOUNDING_BOX_HALF_WIDTH),\(latitud - BOUNDING_BOX_HALF_HEIGHT),\(longitud + BOUNDING_BOX_HALF_WIDTH),\(latitud + BOUNDING_BOX_HALF_HEIGHT)"
     }
     
 }
+
+/* This extension was added as a fix based on student comments */
+extension ViewController {
+    func dismissAnyVisibleKeyboards() {
+        if phraseTextField.isFirstResponder() || latitudeTextField.isFirstResponder() || longitudeTextField.isFirstResponder() {
+            self.view.endEditing(true)
+        }
+    }
+}
+
+
 
